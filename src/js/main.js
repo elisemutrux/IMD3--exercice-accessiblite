b7f513892a1376393f17ff9fc1898e356d5109f7
iwolfisberg <iris.wolfisberg@edufr.ch>
2024-12-18 12:43:35 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const formSubmitBtn = document.querySelector(".submit-btn");
+const formA11y = document.querySelector("#form-a11y");
+formSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
